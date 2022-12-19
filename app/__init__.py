67082182b07fba0a949bdaafd82d5d7ab08f2fc7
MeowthPxnk <meowthpxnk@gmail.com>
2022-12-19 15:39:24 +1000

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,25 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
+
 from app.console.console_logs import ConsoleLogs
 
+
 app = Flask(__name__)
+
+CORS(app)
+cors = CORS(app, resources = {
+    r"/*":{
+        "origins": "*"
+    }
+})
 
 app.config.from_object('app.config.Config')
 socket = SocketIO(app, cors_allowed_origins="*")
 
-
 app_ctx = app.app_context()
 app_ctx.push()
-
-host = "127.0.0.1"
-port = 5000
 
 db = SQLAlchemy(app)
 
---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+POSTGRES_URI = os.getenv("POSTGRES_URI")
+
+class Config():
+    SECRET_KEY = API_KEY
+    SQLALCHEMY_DATABASE_URI = POSTGRES_URI
+    UPLOAD_FOLDER = 'images'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
