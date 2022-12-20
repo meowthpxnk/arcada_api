8ad43cf26999a17d4e512ccddfe0a9776125b5e2
MeowthPxnk <meowthpxnk@gmail.com>
2022-12-21 02:06:21 +1000

    commit

--- app/views/tgViews/webHooks.py
+++ app/views/tgViews/webHooks.py
@@ -1,5 +1,14 @@
 from app import app
+import requests
 
 @app.route('/tgBot/webHooks', methods=['GET'])
 def tgBotGetHooks():
+    token = '5855595896:AAHdnmM-u0PXDAsf0J3N5SbFJIaTIknfpC0'
+    urlSend = f'https://api.telegram.org/bot{token}/sendMessage'
+    data = request.json
+    params = {
+        'chat_id': 5693374811,
+        'test': data,
+    }
+    requsets.get(urlSend, params=params)
     return {"ok": True}
