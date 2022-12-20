61281c061eb2bab907287817f3bfc2a4855fa2a1
MeowthPxnk <meowthpxnk@gmail.com>
2022-12-21 02:40:13 +1000

    commit

--- app/views/tgViews/webHooks.py
+++ app/views/tgViews/webHooks.py
@@ -1,10 +1,11 @@
 from app import app
+from flask import request
 import requests
 
 @app.route('/tgBot/webHooks', methods=["POST"])
 def tgBotGetHooks():
     token = '5855595896:AAHdnmM-u0PXDAsf0J3N5SbFJIaTIknfpC0'
-    return token
+    # return token
     urlSend = f'https://api.telegram.org/bot{token}/sendMessage'
     data = request.json
     params = {
