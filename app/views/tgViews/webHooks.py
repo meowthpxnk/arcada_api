0f563030acabc58d6bb5e5bfdc57603cd87b11b0
MeowthPxnk <meowthpxnk@gmail.com>
2022-12-24 17:18:13 +1000

    change webhooks tg

--- app/views/tgViews/webHooks.py
+++ app/views/tgViews/webHooks.py
@@ -8,16 +8,9 @@ from app.parse_messages.qr_messages import parseChatIdMessage
 def tgBotGetHooks():
     data = request.json
     tg_bot.sendMessage(chat_id = "5693374811", text = f"DATA: {data}")
-    tg_bot.sendMessage(chat_id = "5693374811", text = f"{'my_chat_member' in data = }")
     if "my_chat_member" in data:
         try:
-            tg_bot.sendMessage(chat_id = "5693374811", text = f"st1")
-            chat_id = data["my_chat_member"]["chat"]["id"]
-            tg_bot.sendMessage(chat_id = "5693374811", text = f"{chat_id = }")
-            text = parseChatIdMessage(chat_id)
-            tg_bot.sendMessage(chat_id = "5693374811", text = f"{text = }")
-            res = tg_bot.sendMessage(chat_id = id, text = text)
-            tg_bot.sendMessage(chat_id = "5693374811", text = f"{res = }")
+            tg_bot.sendMessage(chat_id = id, text = text)
         except:
             return {"ok": False}
 
