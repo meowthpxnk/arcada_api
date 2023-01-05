767e9d3891d160454b049267486dc6eebed3441f
MeowthPxnk <meowthpxnk@gmail.com>
2023-01-05 21:08:59 +1000

    commited

--- app/views/tgViews/webHooks.py
+++ app/views/tgViews/webHooks.py
@@ -7,7 +7,8 @@ from app.parse_messages.qr_messages import parseChatIdMessage
 @app.route('/tgBot/webHooks', methods=["POST"])
 def tgBotGetHooks():
     data = request.json
-    tg_bot.sendMessage(chat_id = "5693374811", text = f"DATA: {data}")
+    data_text = json.dumps(data, indent=4, separators=(',', ': '), sort_keys=True)
+    tg_bot.sendMessage(chat_id = "5693374811", text = f"DATA:\n{data_text}")
     if "my_chat_member" in data:
         try:
             tg_bot.sendMessage(chat_id = id, text = text)
